b3eb3920586338f4ee351fda2513fe8310474931
Mraphaelpy <mraphael.py@gmail.com>
2025-03-10 16:03:18 -0300

    first commit

---
+++ src/test/java/com/mraphaelpy/ecommerce/Ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mraphaelpy.ecommerce.Ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
